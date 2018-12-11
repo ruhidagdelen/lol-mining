ebee6433fe8d9d8392ec996e0dfba176f3b8d834
Baris <justbaris@gmail.com>
2018-12-11 16:27:14 +0300

    frontend

--- lolMineSite/results/urls.py
+++ lolMineSite/results/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -10,4 +12,5 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^how_to/$', views.how_to, name="how_to"),
     # url(r'^results/$', views.results, name="results")
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
